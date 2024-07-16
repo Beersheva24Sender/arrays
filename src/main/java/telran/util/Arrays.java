a4a3fb5b02d36bbed92bf9ce48703aa91909f900
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-07-16 18:27:27 +0300

    add sorting mathods

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -2,4 +2,79 @@ package telran.util;
 
 public class Arrays {
 
+    public static int search(int[] ar, int key) {
+        int index = 0;
+        while (index < ar.length && ar[index] != key) {
+            index++;
+        }
+        return index == ar.length ? -1 : index;
+    }
+
+    public static int[] add(int[] ar, int number) {
+        int[] res = java.util.Arrays.copyOf(ar, ar.length + 1);
+        res[ar.length] = number;
+        return res;
+    }
+
+    public static int[] insert(int[] ar, int index, int number) {
+        int[] res = java.util.Arrays.copyOf(ar, ar.length + 1);
+        System.arraycopy(ar, index, res, index + 1, ar.length - index);
+        res[index] = number;
+        return res;
+    }
+
+    public static int[] remove(int[] numbers, int index) {
+        int[] res = java.util.Arrays.copyOf(numbers, numbers.length - 1);
+        System.arraycopy(numbers, index + 1, res, index, res.length - index);
+        return res;
+    }
+
+    private static boolean pushMaxAtEnd(int[] array, int length) {
+        boolean res = true;
+        for (int i = 1; i < length; i++) {
+            if (array[i - 1] > array[i]) {
+                res = false;
+                swap(array, i - 1, i);
+            }
+        }
+        return res;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void sort(int[] array) {
+        int length = array.length;
+        boolean flSorted = false;
+        while (!flSorted) {
+            length--;
+            flSorted = pushMaxAtEnd(array, length);
+        }
+    }
+
+    public static int binarySearch(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+        int middle = (left + right) / 2;
+
+        while (middle != right && middle != left) {
+            if (array[middle] == key) {
+                return middle;
+            } else if (array[left] == key) {
+                return left;
+            } else if (array[right] == key) {
+                return right;
+            } else if (key < array[middle]) {
+                right = middle;
+                middle = (left + right) / 2;
+            } else {
+                left = middle;
+                middle = (left + right) / 2;
+            }
+        }
+        return -1;
+    }
 }
---
+++ src/test/java/telran/util/test/ArrayTest.java
@@ -0,0 +1,103 @@
+package telran.util.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static telran.util.Arrays.*;
+
+import java.util.Random;
+
+public class ArrayTest {
+
+    private static final int N_ELEMENTS = 1_000;
+    int[] numbers = { 10, 7, 12, -4, 13, 3, 14 };
+
+    @Test
+    void searchTest() {
+        assertEquals(0, search(numbers, 10));
+        assertEquals(6, search(numbers, 14));
+        assertEquals(3, search(numbers, -4));
+        assertEquals(-1, search(numbers, 100));
+    }
+
+    @Test
+    void addTest() {
+        int newNumber = 100;
+        int[] expected = { 10, 7, 12, -4, 13, 3, 14, newNumber };
+        assertArrayEquals(expected, add(numbers, newNumber));
+    }
+
+    @Test
+    void insertTest() {
+        // {10, 7, 12, -4, 13, 3, 14} - all numbers
+        int newNumber = 30;
+        int[] expected_0 = { newNumber, 10, 7, 12, -4, 13, 3, 14 };
+        int[] expected_3 = { 10, 7, 12, newNumber, -4, 13, 3, 14 };
+        int[] expected_last = { 10, 7, 12, -4, 13, 3, 14, newNumber };
+        assertArrayEquals(expected_0, insert(numbers, 0, newNumber));
+        assertArrayEquals(expected_3, insert(numbers, 3, newNumber));
+        assertArrayEquals(expected_last, insert(numbers, numbers.length, newNumber));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> insert(numbers, numbers.length + 1, newNumber));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> insert(numbers, -1, newNumber));
+    }
+
+    @Test
+    void removeTest() {
+        // {10, 7, 12, -4, 13, 3, 14} - all numbers
+        int[] expected_0 = { 7, 12, -4, 13, 3, 14 };
+        int[] expected_3 = { 10, 7, 12, 13, 3, 14 };
+        int[] expected_last = { 10, 7, 12, -4, 13, 3 };
+        assertArrayEquals(expected_0, remove(numbers, 0));
+        assertArrayEquals(expected_3, remove(numbers, 3));
+        assertArrayEquals(expected_last, remove(numbers, numbers.length - 1));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> remove(numbers, numbers.length));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> remove(numbers, -1));
+    }
+
+    @Test
+    void sortTest() {
+        int[] testNumbers = java.util.Arrays.copyOf(numbers, numbers.length);
+        int[] expected = { -4, 3, 7, 10, 12, 13, 14 };
+        sort(testNumbers);
+        assertArrayEquals(expected, testNumbers);
+    }
+
+    @Test
+    void sortTestRandomArray() {
+        int[] array = getRandomArray(N_ELEMENTS);
+        int limit = array.length - 1;
+        sort(array);
+        for (int i = 1; i < limit; i++) {
+            assertTrue(array[i - 1] <= array[i]);
+        }
+    }
+
+    private int[] getRandomArray(int nElements) {
+        int[] res = new int[nElements];
+        Random random = new Random();
+        for (int i = 0; i < nElements; i++) {
+            res[i] = random.nextInt();
+        }
+        return res;
+    }
+
+    @Test
+    void binarySearchTest() {
+        int[] array = { -4, 0, 3, 10, 12, 13, 14, 15, 16, 17 };
+
+        // if the key exists in the array
+        assertEquals(0, binarySearch(array, -4));
+        assertEquals(1, binarySearch(array, 0));
+        assertEquals(3, binarySearch(array, 10));
+        assertEquals(7, binarySearch(array, 15));
+        assertEquals(9, binarySearch(array, 17));
+
+        // if the key does not exist in the array
+        int key = -1;
+        int index = -1;
+        assertEquals(index, binarySearch(array, key));
+    }
+}
